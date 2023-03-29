4fc3468d5836c6282abf5c794eda57eae28395ad
JarSeal <kai.forsman@gmail.com>
2023-03-29 22:20:16 +0200

    Add compo and router logger helpers as exports

--- src/Lighter/index.js
+++ src/Lighter/index.js
@@ -1,6 +1,26 @@
-import Component from './Component';
-import Router from './Router';
+import Component, {
+  isLoggerQuiet as isComponentLoggerQuiet,
+  setLoggerCallback as setComponentLoggerCallback,
+} from './Component';
+import Router, {
+  isLoggerQuiet as isRouterLoggerQuiet,
+  setLoggerCallback as setRouterLoggerCallback,
+  RouterRef,
+} from './Router';
 import State from './State';
 import { LocalStorage, SessionStorage, Logger, createUUID } from './utils';
 
-export { Component, Router, State, LocalStorage, SessionStorage, Logger, createUUID };
+export {
+  Component,
+  Router,
+  State,
+  LocalStorage,
+  SessionStorage,
+  Logger,
+  createUUID,
+  isComponentLoggerQuiet,
+  setComponentLoggerCallback,
+  isRouterLoggerQuiet,
+  setRouterLoggerCallback,
+  RouterRef,
+};
