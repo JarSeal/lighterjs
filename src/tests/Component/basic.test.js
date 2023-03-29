38366d73ba3cf4a07c2286d95b4bc18c1b55beec
JarSeal <kai.forsman@gmail.com>
2023-03-29 22:16:45 +0200

    Add more component tests

--- src/tests/Component/basic.test.js
+++ src/tests/Component/basic.test.js
@@ -1,6 +1,12 @@
-import { Component } from '../../LIGHTER';
+import { Component, isComponentLoggerQuiet } from '../../LIGHTER';
+import { createEmptyRootDiv, isUUID } from '../testUtils';
 
 describe('Basic Component tests', () => {
+  beforeAll(() => {
+    isComponentLoggerQuiet(true);
+    createEmptyRootDiv();
+  });
+
   it('should create a basic text div', () => {
     const testText = 'Test text';
     const comp = new Component({ text: testText });
@@ -18,13 +24,68 @@ describe('Basic Component tests', () => {
     expect(comp.isComponent).toBeTruthy();
     expect(comp.children).toEqual({});
     expect(comp.listeners).toEqual({});
-    console.log('comp', comp);
     expect(comp.listenersToAdd).toEqual([]);
-    // drawing = false
-    // discarding = false
-    // router = null
-    // id = isUUID
-    // props.id = isUUID
-    // props.text = testText
+    expect(comp.drawing).toBeFalsy();
+    expect(comp.discarding).toBeFalsy();
+    expect(comp.router).toEqual(null);
+    expect(isUUID(comp.id)).toBeTruthy();
+    expect(isUUID(comp.props.id)).toBeTruthy();
+    expect(comp.props.text).toEqual(testText);
   });
+
+  it('should fail when trying to draw a component without a parent or attachId', () => {
+    const comp = new Component();
+    let errorMsg = '';
+    try {
+      comp.draw();
+    } catch (err) {
+      errorMsg = err.message;
+    }
+    expect(errorMsg).toEqual('Call stack');
+  });
+
+  it('should create an appRoot component with another component as a child and draw them to DOM', () => {
+    const appRoot = new Component({ id: 'appRoot', attachId: 'root' });
+    appRoot.draw();
+    appRoot.add(new Component({ id: 'main-menu', tag: 'menu', text: 'Menu' })).draw();
+    expect(appRoot.id).toEqual('appRoot');
+    expect(appRoot.props.id).toEqual('appRoot');
+    expect(appRoot.props.attachId).toEqual('root');
+    expect(Object.keys(appRoot.children).length).toEqual(1);
+    expect(appRoot.children['main-menu'].parent).toEqual(appRoot);
+    expect(appRoot.children['main-menu'].id).toEqual('main-menu');
+    expect(appRoot.children['main-menu'].props.id).toEqual('main-menu');
+    expect(appRoot.children['main-menu'].props.tag).toEqual('menu');
+    expect(appRoot.children['main-menu'].props.text).toEqual('Menu');
+
+    const appRootElem = document.getElementById('root').children.item(0);
+    const menuElem = appRootElem.children.item(0);
+    expect(appRootElem.tagName).toEqual('DIV');
+    expect(appRootElem.children.length).toEqual(1);
+    expect(menuElem.tagName).toEqual('MENU');
+    expect(menuElem.innerText).toEqual('Menu');
+
+    appRoot.discard(true);
+    expect(document.getElementById('root').children.length).toEqual(0);
+  });
+
+  it('should create an appRoot component with attributes', () => {
+    const appRoot = new Component({
+      _id: 'appRoot',
+      attachId: 'root',
+      attributes: { 'data-count': 12, test: 'some text' },
+      style: { color: 'red', maxWidth: '12px' },
+      classes: ['myClass', 'anotherClass'],
+    });
+    appRoot.draw();
+    expect(appRoot.elem.getAttribute('id')).toEqual('appRoot');
+    expect(appRoot.elem.getAttribute('data-count')).toEqual('12');
+    expect(appRoot.elem.getAttribute('test')).toEqual('some text');
+    expect(appRoot.elem.style.color).toEqual('red');
+    expect(appRoot.elem.style.maxWidth).toEqual('12px');
+    expect(appRoot.elem.classList.contains('myClass')).toBeTruthy();
+    expect(appRoot.elem.classList.contains('anotherClass')).toBeTruthy();
+  });
+
+  // Test for updating component props with new draw
 });
--- src/tests/testUtils.js
+++ src/tests/testUtils.js
@@ -1,5 +1,9 @@
-export const checkUUID = (text) => {
+export const isUUID = (text) => {
   const regexExp =
     /^[0-9a-fA-F]{8}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{4}\b-[0-9a-fA-F]{12}$/gi;
   return regexExp.test(text);
+};
+
+export const createEmptyRootDiv = () => {
+  document.body.innerHTML = `<div id="root"></div>`;
 };
