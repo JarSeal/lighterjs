1c524e8c9f1267276753789b4d314bb0dde982ae
JarSeal <kai.forsman@gmail.com>
2023-04-14 00:51:08 +0200

    More InputElem props and examples

--- src/examples/Components/BasicComponents/InputText.js
+++ src/examples/Components/BasicComponents/InputText.js
@@ -4,10 +4,11 @@ import { Component } from '../../../Lighter';
 // - label: string/template (input field's label string)
 // - value: string (input field's value)
 // - maxlength: number (input field's max length of characters)
+// - password: boolean (whether the input elem's type is password or not, default false = type="text")
+// - disabled: boolean (whether the input elem is disabled or not)
 // - onChange: function(e, value, this) (input field's on change listener callback)
 // - onFocus: function(event, value, this) (input field's on focus listener callback)
 // - onBlur: function(event, value, this) (input field's on blur listener callback)
-// - required: boolean (whether an input is required or not, default false)
 // - focusOnFirstDraw = boolean (whether the input is focused after the first drawing or not, default false)
 class InputText extends Component {
   constructor(props) {
@@ -15,12 +16,14 @@ class InputText extends Component {
     this.label = props.label || '';
     this.value = props.value || '';
     this.maxlength = props.maxlength || null;
-    this.required = props.required || false;
+    this.password = props.password || false;
     this.focusOnFirstDraw = props.focusOnFirstDraw || false;
+    this.changeHappened = false;
     this.props.template = `<div class="inputText formElem${this.label ? '' : ' noLabel'}">
       <label class="inputTextInner inputInner" for="${this.id}">
         <span class="inputTextLabel inputLabel">${this.label}</span>
-        <input class="input" type="text" value="${this.value}" id="${this.id}"
+        <input class="input" type="${this.password ? 'password' : 'text'}"
+          value="${this.value}" id="${this.id}"
           ${this.maxlength ? `maxlength=${this.maxlength}` : ''}
         />
       </label>
@@ -29,24 +32,55 @@ class InputText extends Component {
   }
 
   paint = () => {
-    if (this.focusOnFirstDraw) this.elem.querySelector('.input').focus();
+    if (this.props.disabled) {
+      this.disabled = true;
+      this.getInputElem().disabled = true;
+      this.elem.classList.add('disabled');
+    } else {
+      this.disabled = false;
+      this.getInputElem().disabled = false;
+      this.elem.classList.remove('disabled');
+    }
+    if (this.focusOnFirstDraw) this.getInputElem().focus();
+  };
+
+  // msg: string (error message to show with the component)
+  showError = (msg) => {
+    if (!msg) {
+      this.noErrors();
+      return;
+    }
+    const errorElem = this.elem.querySelector('.inputErrorMsg');
+    errorElem.textContent = msg;
+    this.elem.classList.add('error');
+  };
+
+  noErrors = () => {
+    const errorElem = this.elem.querySelector('.inputErrorMsg');
+    errorElem.textContent = '';
+    this.elem.classList.remove('error');
+  };
+
+  getInputElem = () => {
+    if (this.inputElem) return this.inputElem;
+    this.inputElem = this.elem.querySelector('.input');
+    return this.inputElem;
   };
 
   addListeners = () => {
-    const inputElem = this.elem.querySelector('.input');
-    if (this.props.onChange) {
-      this.addListener({
-        id: 'onchange',
-        target: inputElem,
-        type: 'keyup',
-        fn: (e) => {
-          const value = e.target.value;
-          if (this.value === value) return;
-          this.value = value;
-          this.props.onChange(e, value, this);
-        },
-      });
-    }
+    const inputElem = this.getInputElem();
+    this.addListener({
+      id: 'onchange',
+      target: inputElem,
+      type: 'keyup',
+      fn: (e) => {
+        const value = e.target.value;
+        if (this.value === value) return;
+        this.changeHappened = true;
+        this.value = value;
+        this.props.onChange(e, value, this);
+      },
+    });
     this.addListener({
       id: 'focus',
       target: inputElem,
--- src/examples/Components/BasicComponents/examples/InputTextExample.js
+++ src/examples/Components/BasicComponents/examples/InputTextExample.js
@@ -4,11 +4,34 @@ const InputTextExample = (parent, InputText) => {
     attachId,
     onChange: (e, value) => console.log(e, value),
   };
+  const inputElem1 = parent
+    .add(
+      new InputText({
+        ...commonProps,
+        label: 'Label',
+        maxlength: 5,
+        focusOnFirstDraw: true,
+        onChange: (_, value) => {
+          if (value) {
+            inputElem1.noErrors();
+            return;
+          }
+          inputElem1.showError('Required');
+        },
+      })
+    )
+    .draw();
+  inputElem1.showError('Required');
+  parent.add({ attachId, attributes: { style: 'height: 30px;' } }).draw();
+  parent.add(new InputText({ ...commonProps, label: 'With value', value: 'some text' })).draw();
+  parent.add({ attachId, attributes: { style: 'height: 30px;' } }).draw();
   parent
-    .add(new InputText({ ...commonProps, label: 'Label', maxlength: '3', focusOnFirstDraw: true }))
+    .add(new InputText({ ...commonProps, label: 'Password', password: true, value: 'password' }))
     .draw();
   parent.add({ attachId, attributes: { style: 'height: 30px;' } }).draw();
-  parent.add(new InputText({ ...commonProps, label: 'Label', value: 'some text' })).draw();
+  parent
+    .add(new InputText({ ...commonProps, label: 'Disabled', value: 'some text', disabled: true }))
+    .draw();
 };
 
 export default InputTextExample;
