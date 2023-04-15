759efc66f824a94c1dac03c0325a187f5960cbca
JarSeal <kai.forsman@gmail.com>
2023-04-15 13:07:10 +0200

    InputDropdown features and examples

--- src/examples/Components/BasicComponents/InputDropdown.js
+++ src/examples/Components/BasicComponents/InputDropdown.js
@@ -3,10 +3,10 @@ import { Component } from '../../../Lighter';
 // Common props:
 // - label: string/template (input field's label string)
 // - value: string (input field's value)
-// - options: (required) [
+// - options: array of objects (required) [
 //     { value: any, text: string/number, disabled: boolean },
 //     or grouped:
-//     { label: string/number, group:
+//     { label: string/number, group: array of objects
 //       [
 //         { value: any, text: string/number, disabled: boolean }
 //       ]
@@ -24,7 +24,8 @@ class InputDropdown extends Component {
     this.value = props.value || '';
     this.options = props.options;
     if (!props.options?.length) {
-      // @TODO: Add warning
+      console.warn('options prop missing for id: ' + this.id);
+      this.options = this.props.options = {};
     }
     this.focusOnFirstDraw = props.focusOnFirstDraw || false;
     this.changeHappened = false;
@@ -41,7 +42,110 @@ class InputDropdown extends Component {
     </div>`;
   }
 
-  _createOptions = () => {};
+  paint = () => {
+    this.disabled = this.props.disabled;
+    this.getInputElem().disabled = this.disabled;
+    this.disabled ? this.elem.classList.add('disabled') : this.elem.classList.remove('disabled');
+    if (this.focusOnFirstDraw) this.getInputElem().focus();
+  };
+
+  // msg: string (error message to show with the component)
+  showError = (msg) => {
+    if (!msg) {
+      this.clearErrors();
+      return;
+    }
+    const errorElem = this.elem.querySelector('.inputErrorMsg');
+    errorElem.textContent = msg;
+    this.elem.classList.add('error');
+  };
+
+  clearErrors = () => {
+    const errorElem = this.elem.querySelector('.inputErrorMsg');
+    errorElem.textContent = '';
+    this.elem.classList.remove('error');
+  };
+
+  getInputElem = () => {
+    if (this.inputElem) return this.inputElem;
+    this.inputElem = this.elem.querySelector('.inputElem');
+    return this.inputElem;
+  };
+
+  addListeners = () => {
+    const inputElem = this.getInputElem();
+    this.addListener({
+      id: 'onchange',
+      target: inputElem,
+      type: 'change',
+      fn: (e) => {
+        const value = e.target.value;
+        if (this.value === value) return;
+        console.log('Changed');
+        this.changeHappened = true;
+        this.value = value;
+        if (this.props.onChange) this.props.onChange(e, value, this);
+      },
+    });
+    this.addListener({
+      id: 'focus',
+      target: inputElem,
+      type: 'focus',
+      fn: (e) => {
+        this.elem.classList.add('focus');
+        if (this.props.onFocus) {
+          this.props.onFocus(e, this.value, this);
+        }
+      },
+    });
+    this.addListener({
+      id: 'blur',
+      target: inputElem,
+      type: 'blur',
+      fn: (e) => {
+        this.elem.classList.remove('focus');
+        if (this.props.onBlur) {
+          this.props.onBlur(e, this.value, this);
+        }
+      },
+    });
+  };
+
+  setValue = (newValue) => {
+    this.value = newValue;
+    this.props.value = newValue;
+    const optionsTags = this.elem.querySelectorAll('option');
+    for (let i = 0; i < optionsTags.length; i++) {
+      optionsTags[i].removeAttribute('selected');
+      if (String(newValue) === optionsTags[i].value) {
+        optionsTags[i].setAttribute('selected', 'true');
+      }
+    }
+  };
+
+  _createOptions = () => {
+    const { options } = this.props;
+    let optionsStr = '';
+    const addOption = (props) => {
+      optionsStr += `<option
+        ${props.disabled ? ' disabled' : ''}
+        value="${props.value}"
+        ${this.value === props.value ? ' selected' : ''}
+      >${props.text}</option>`;
+    };
+    for (let i = 0; i < options.length; i++) {
+      if (options[i].group) {
+        optionsStr += `<optgroup label="${options[i].label}">`;
+        for (let j = 0; j < options[i].group.length; j++) {
+          addOption(options[i].group[j]);
+        }
+        optionsStr += '</optgroup>';
+        continue;
+      }
+      addOption(options[i]);
+    }
+    return optionsStr;
+  };
 }
 
 export default InputDropdown;
--- src/examples/Components/BasicComponents/examples/InputDropdownExamples.js
+++ src/examples/Components/BasicComponents/examples/InputDropdownExamples.js
@@ -2,7 +2,86 @@ const InputDropdownExample = (parent, InputDropdown) => {
   const attachId = 'examples';
   const commonProps = { attachId };
   const breakProps = { attachId, attributes: { style: 'height: 30px;' } };
-  parent.add(new InputDropdown({ ...commonProps, text: 'YYH' })).draw();
+  const options1 = [
+    { value: 1, text: 'First choice' },
+    { value: 2, text: 'Second choice' },
+    { value: 3, text: 'Third choice' },
+    { value: 4, text: 'Fourth choice' },
+    { value: 5, text: 'Fifth choice' },
+  ];
+  const options2 = [
+    {
+      label: 'Group 1',
+      group: [
+        { value: 1, text: 'First choice' },
+        { value: 2, text: 'Second choice' },
+        { value: 3, text: 'Third choice' },
+        { value: 4, text: 'Fourth choice' },
+        { value: 5, text: 'Fifth choice' },
+      ],
+    },
+    {
+      label: 'Group 2',
+      group: [
+        { value: 6, text: 'Sixth choice' },
+        { value: 7, text: 'Seventh choice' },
+        { value: 8, text: 'Eight choice' },
+        { value: 9, text: 'Ninth choice' },
+        { value: 10, text: 'Tenth choice' },
+      ],
+    },
+  ];
+  parent
+    .add(new InputDropdown({ ...commonProps, options: options1, value: 3, label: 'Label' }))
+    .draw();
+  parent.add(breakProps).draw();
+  parent
+    .add(
+      new InputDropdown({
+        ...commonProps,
+        options: options1,
+        value: 3,
+        label: 'Disabled',
+        disabled: true,
+      })
+    )
+    .draw();
+  parent.add(breakProps).draw();
+  parent
+    .add(
+      new InputDropdown({
+        ...commonProps,
+        options: options2,
+        value: 7,
+        label: 'Grouped',
+      })
+    )
+    .draw();
+  parent.add(breakProps).draw();
+  const dropdown1 = parent
+    .add(
+      new InputDropdown({
+        ...commonProps,
+        options: options1,
+        value: 3,
+        label: 'When this one changes...',
+        onChange: (_, value) => {
+          dropdown2.setValue(value);
+        },
+      })
+    )
+    .draw();
+  parent.add(breakProps).draw();
+  const dropdown2 = parent
+    .add(
+      new InputDropdown({
+        ...commonProps,
+        options: options1,
+        value: dropdown1.value,
+        label: '...so does this',
+      })
+    )
+    .draw();
 };
 
 export default InputDropdownExample;
