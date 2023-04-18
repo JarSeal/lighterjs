1890033dbadfd0e2e66e8d63baf230d7619f8a66
JarSeal <kai.forsman@gmail.com>
2023-04-18 23:50:40 +0200

    Dialog improvements and features

--- src/examples/Components/BasicComponents/Dialog.js
+++ src/examples/Components/BasicComponents/Dialog.js
@@ -1,17 +1,28 @@
 import { Component } from '../../../Lighter';
 
 // props:
-// - animTime: number[showTime, hideTime] (number of milliseconds for the bg and dialog to appear/hide, default [200, 200])
+// - animTime: number[showTime, hideTime] (number of milliseconds for the bg and dialog to appear/hide, default [400, 200])
 // - inlineStyles: boolean (whether to use basic inline CSS styles for dialog, background, showing, and hiding)
+// - windowConfirmMessage: string (window.confirm message when promptForLosingChanges is true)
+// - closeButtonTemplate: string/template (close button string, template, or icon)
 class Dialog extends Component {
   constructor(props) {
     super(props);
     this.defaultAnimTime = [400, 200];
     this.animTime = props.animTime || this.defaultAnimTime;
     this.inlineStyles = props.inlineStyles || false;
-    this.dialogContent = null;
+    this.closeButton = null;
     this.dialogTitle = null;
+    this.dialogContent = null;
     this.outsideClickEnabled = true;
+    this.hasChanges = false;
+    this.title = null;
+    this.hideCloseButton = false;
+    this.promptOnClose = false;
+    this.closeButtonTemplate = props.closeButtonTemplate || 'x';
+    this.windowConfirmMessage =
+      props.windowConfirmMessage ||
+      'You have will lose the changes you made in the dialog. Are you sure?';
     this.props.template = `<div class="dialogOuter"
       ${this.inlineStyles ? ` style="${this._outerStyles('hide')}"` : ''}
     >
@@ -25,53 +36,75 @@ class Dialog extends Component {
     dialogRefs[this.id] = this;
   }
 
-  addListeners = () => {
+  // dialogData:
+  // - component: Component (the content to display, required)
+  // - props?: object (Component props to use)
+  // - title?: string (Dialog title)
+  // - @TODO: outerClasses?: [] (.dialogOuter extra classes)
+  // - hideCloseButton?: boolean (whether to hide close button, default false)
+  // - outsideClickEnabled?: boolean (whether the outside click is enabled or not, default true)
+  // - promptOnClose?: boolean (whether the user is prompted for leaving the dialog with hasChanges = true, default false)
+  // - @TODO: buttons?: array [ (the buttons to show at the bottom of the dialog, default no buttons)
+  //   - { label: string/template, onClick: function(dialog), classes?: [] }
+  //   ]
+  // - @TODO: stickyButtons?: same as buttons but they will be rendered outside the .dialogContent
+  show = (dialogData) => {
+    if (!dialogData.component) {
+      console.error('No component provided for the dialog to show.');
+      return;
+    }
+    clearTimeout(this.afterAnimOut);
     this.addListener({
       id: 'outside-click',
       target: this.elem.querySelector('.dialogBackground'),
       type: 'click',
-      fn: () => {
-        if (this.outsideClickEnabled) this.hide();
-      },
+      fn: () => (this.outsideClickEnabled ? this.close() : null),
     });
-  };
-
-  // dialogData:
-  // - component: Component (the content to display)
-  // - props?: object (Component props to use)
-  // - title?: string (Dialog title)
-  // - showCloseButton?: boolean (whether to show close button, default true)
-  // - showCancelButton?: boolean (whether to show cancel button, default false)
-  // - outsideClickEnabled?: boolean (whether the outside click is enabled or not, default true)
-  show = (dialogData) => {
-    if (dialogData.title) this.setTitle(dialogData.title);
-    // @TODO: show close button
-    // @TODO: show cancel button
-    if (dialogData.outsideClickEnabled) this.outsideClickEnabled = dialogData.outsideClickEnabled;
+    this.title = dialogData.title || null;
+    this.hideCloseButton = dialogData.hideCloseButton || false;
+    this.promptOnClose = dialogData.promptOnClose || false;
+    this.outsideClickEnabled = dialogData.outsideClickEnabled !== false;
     if (this.dialogContent?.isComponent) this.dialogContent.discard(true);
-    this.dialogContent = new dialogData.component({
-      ...(dialogData.props || {}),
+    this.dialogContent = new Component({
+      classes: ['dialogContent'],
       attachId: this.id + '-dialog',
     });
-    console.log(this.dialogContent);
     this.dialogContent.draw();
+    this.dialogContent.add(new dialogData.component({ ...(dialogData.props || {}) })).draw();
+    if (this.title) this.setTitle(this.title);
+    if (!this.hideCloseButton) this.createCloseButton();
+    this.elem.querySelector('.dialogBackground').style.transitionDuration = this.animTime[0] + 'ms';
+    this.elem.querySelector('.dialog').style.transitionDuration = this.animTime[0] + 'ms';
     if (this.inlineStyles) this.elem.style.cssText = this._outerStyles('show');
     this.elem.classList.add('showing');
     this.elem.classList.add('show');
-    setTimeout(() => {
+    this.afterAnimIn = setTimeout(() => {
       this.elem.classList.remove('showing');
     }, this.animTime[0]);
   };
 
-  hide = () => {
+  close = () => {
+    if (this.hasChanges && this.promptOnClose && !window.confirm(this.windowConfirmMessage)) {
+      return;
+    }
+    clearTimeout(this.afterAnimIn);
+    this.removeListener('outside-click');
+    this.elem.querySelector('.dialogBackground').style.transitionDuration = this.animTime[1] + 'ms';
+    this.elem.querySelector('.dialog').style.transitionDuration = this.animTime[1] + 'ms';
+    this.elem.classList.remove('showing');
     this.elem.classList.add('hiding');
-    setTimeout(() => {
-      this.elem.classList.remove('showing');
+    this.hasChanges = false;
+    this.title = null;
+    this.hideCloseButton = false;
+    this.promptOnClose = false;
+    this.afterAnimOut = setTimeout(() => {
       this.elem.classList.remove('show');
       this.elem.classList.remove('hiding');
       if (this.inlineStyles) this.elem.style.cssText = this._outerStyles('hide');
+      if (this.closeButton?.isComponent) this.closeButton.discard(true);
       if (this.dialogTitle?.isComponent) this.dialogTitle.discard(true);
       if (this.dialogContent?.isComponent) this.dialogContent.discard(true);
+      this.closeButton = null;
       this.dialogTitle = null;
       this.dialogContent = null;
     }, this.animTime[1]);
@@ -84,9 +117,26 @@ class Dialog extends Component {
       attachId: this.id + '-dialog',
       text: title,
       tag: 'h2',
-      class: 'dialogTitle',
+      classes: ['dialogTitle'],
+      prepend: true,
     });
     this.dialogTitle.draw();
+  };
+
+  createCloseButton = () => {
+    if (this.closeButton?.isComponent) this.closeButton.discard(true);
+    this.closeButton = new Component({
+      attachId: this.id + '-dialog',
+      classes: ['dialogCloseButton'],
+      template: `<button>${this.closeButtonTemplate}</button>`,
+      prepend: true,
+    });
+    this.closeButton.draw();
+    this.closeButton.addListener({
+      id: 'close-button-click',
+      type: 'click',
+      fn: () => this.close(),
+    });
   };
 
   removeRef = () => {
--- src/examples/Components/BasicComponents/examples/DialogExample.js
+++ src/examples/Components/BasicComponents/examples/DialogExample.js
@@ -1,11 +1,12 @@
 import { Component } from '../../../../Lighter';
 import Button from '../Button';
 import { useDialog } from '../Dialog';
+import InputText from '../InputText';
 
 const DialogExample = (parent, Dialog) => {
   if (useDialog('dialogId')) useDialog('dialogId').removeRef();
   const attachId = 'examples';
-  // document.getElementById('examples').style.minHeight = '500px';
+  const breakProps = { attachId, attributes: { style: 'height: 30px;' } };
 
   parent.add(new Dialog({ attachId: 'root', id: 'dialogId', inlineStyles: true })).draw();
 
@@ -23,6 +24,34 @@ const DialogExample = (parent, Dialog) => {
       })
     )
     .draw();
+  parent.add(breakProps).draw();
+
+  parent
+    .add(
+      new Button({
+        text: 'Custom Component as Dialog',
+        attachId,
+        onClick: () => useDialog().show({ component: ExampleDialog }),
+      })
+    )
+    .draw();
 };
+
+class ExampleDialog extends Component {
+  constructor(props) {
+    super(props);
+    this.dialog = useDialog();
+    this.dialog.promptOnClose = true;
+    this.dialog.title = 'My custom component';
+    this.dialog.outsideClickEnabled = false;
+  }
+
+  paint = () => {
+    this.add({
+      text: 'Making changes to the input field will prompt the user when the dialog is closed.',
+    }).draw();
+    this.add(new InputText({ onChange: () => (this.dialog.hasChanges = true) })).draw();
+  };
+}
 
 export default DialogExample;
