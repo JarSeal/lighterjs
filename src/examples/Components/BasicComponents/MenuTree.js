25698882d497d585b071cac57a641af628a6135e
JarSeal <kai.forsman@gmail.com>
2023-04-25 08:11:28 +0200

    Finish MenuTree and examples

--- src/examples/Components/BasicComponents/MenuTree.js
+++ src/examples/Components/BasicComponents/MenuTree.js
@@ -5,13 +5,17 @@ import { Component } from '../../../Lighter';
 //   {
 //     - label: string/template (text or html to display on the menu item)
 //     - link?: string (link string, if also the onClick is defined, the link is called after the onClick)
-//     - onClick?: function() (function for the item click)
+//     - linkTarget?: string (link target attribute)
+//     - routeLink?: string (same as link, but it doesn't force a page refresh, requires router to be set up)
+//     - onClick?: function(e, treeItem) (function for the item click)
 //     - disabled?: boolean (whether the item and its children are disabled or not, default false)
+//     - labelTag?: string (tag of the label, default 'span' but if link is defined, then it is 'a', and if onClick then it is 'button')
 //     - tree?: array of objects (same as the parent tree)
 //   }
 // ]
 // - disabled?: boolean (whether the whole menu tree is disabled or not, default false)
 // - inlineStyles?: boolean (whether basic inline CSS styles are applied or not, default false)
+// - forceUpdateRouteLinks?: boolean (whether to use forceUpdate for routeLinks or not, default true)
 class MenuTree extends Component {
   constructor(props) {
     super(props);
@@ -26,31 +30,79 @@ class MenuTree extends Component {
     this.tree = props.tree;
     this.disabled = props.disabled || false;
     this.inlineStyles = props.inlinestyles || false;
+    this.forceUpdateRouteLinks = props.forceUpdateRouteLinks === false ? false : true;
     if (this.treeComponent?.idComponent) this.treeComponent.discard(true);
   };
 
   paint = (props) => {
     this._defineProps(props);
-    const template = this._createTreeTemplate();
+    const template = this._createTreeTemplate(this.tree);
     this.treeComponent = this.addDraw({ template });
+    this.treeComponent.addListener({
+      id: 'tree-click',
+      type: 'click',
+      fn: this._treeOnClick,
+    });
   };
 
-  _createTreeTemplate = (prevIdPrefix) => {
-    let idPrefix = prevIdPrefix || `mt-${this.id}`;
+  _createTreeTemplate = (tree, prevIdPrefix) => {
+    let idPrefix = prevIdPrefix || '';
     let template = '<ul class="menuTreeGroup">';
-    for (let i = 0; i < this.tree.length; i++) {
-      const itemId = `${idPrefix}__${i}`;
-      template += `<li
-        class="menuTreeItem${this.tree[i].disabled ? ' disabled' : ''}"
-        id="${itemId}"
-      ><span class="menuTreeItemLabel">${this.tree[i].label || ''}</span>`;
-      if (this.tree[i].tree) {
-        template += this._createTreeTemplate(itemId);
+    for (let i = 0; i < tree.length; i++) {
+      const treePos = idPrefix ? idPrefix + ', ' + i : i;
+      const tag = this._getLabelTag(tree[i]);
+      let href = tag === 'a' ? ` href="${tree[i].link}"` : '';
+      href += tag === 'a' && tree[i].target ? ` target="${tree[i].target}"` : '';
+      template += `<li class="menuTreeItem${tree[i].disabled ? ' disabled' : ''}">
+      <${tag}${href} class="menuTreeItemLabel" data-treepos='[${treePos}]'>
+        ${tree[i].label || ''}
+      </${tag}>`;
+      if (tree[i].tree) {
+        template += this._createTreeTemplate(tree[i].tree, treePos);
       }
       template += '</li>';
     }
     template += '</ul>';
     return template;
+  };
+
+  _getLabelTag = (item) => {
+    if (item.tag) return item.tag;
+    if (!item.tag && item.link) return 'a';
+    if (!item.tag && !item.link && (item.onClick || item.routeLink)) return 'button';
+    return 'span';
+  };
+
+  _treeOnClick = (e) => {
+    e.stopPropagation();
+    const elem = e.target;
+    const treePos = JSON.parse(elem.getAttribute('data-treepos'));
+    if (!treePos?.length) return;
+
+    let treeItem = this.tree;
+    let disabled = false;
+    for (let i = 0; i < treePos.length; i++) {
+      const nextIndex = treePos[i];
+      if (treeItem[nextIndex].disabled) disabled = true; // This disables the whole tree or subtree
+      if (i + 1 === treePos.length) {
+        treeItem = treeItem[nextIndex];
+      } else {
+        treeItem = treeItem[nextIndex]?.tree;
+      }
+    }
+
+    if (disabled) {
+      e.preventDefault();
+      return;
+    }
+    if (treeItem.onClick) treeItem.onClick(e, treeItem);
+    if (treeItem.routeLink) {
+      if (!this.router) {
+        console.warn(`Tree item has a routeLink prop but no router is found.`);
+        return;
+      }
+      this.router.changeRoute(treeItem.routeLink, { forceUpdate: this.forceUpdateRouteLinks });
+    }
   };
 }
 
--- src/examples/Components/BasicComponents/examples/MenuTreeExample.js
+++ src/examples/Components/BasicComponents/examples/MenuTreeExample.js
@@ -6,8 +6,66 @@ const MenuTreeExample = (parent, MenuTree) => {
     { label: 'Root item 1', link: '/' },
     { label: 'Root item 2 disabled', link: '/404', disabled: true },
   ];
+  const menuTree2 = [
+    { label: 'Level 1 item 1', onClick: (e, item) => console.log('Click Root item 1', e, item) },
+    {
+      label: 'Level 1 item 2',
+      tree: [
+        { label: 'Level 2 item 1' },
+        {
+          label: 'Level 2 item 2 (disabled onClick)',
+          disabled: true,
+          onClick: (e, item) => console.log('Click should not happen', e, item),
+        },
+        { label: 'Level 2 item 3', link: '/' },
+        {
+          label: 'Level 2 item 4',
+          link: '/',
+          onClick: (e, item) => console.log('Click Level 2 item 4', e, item),
+        },
+      ],
+    },
+    {
+      label: 'Level 1 item 3 (whole sub tree disabled)',
+      onClick: (e, item) => console.log('Click Root item 3', e, item),
+      disabled: true,
+      tree: [{ label: 'Disabled because parent is', link: '/' }],
+    },
+    {
+      label: 'Level 1 item 4',
+      onClick: (e, item) => console.log('Click Root item 4', e, item),
+      tree: [
+        { label: 'Level 2 item 1 (disabled link)', link: '/', disabled: true },
+        {
+          label: 'Level 2 item 1',
+          tree: [
+            { label: 'Level 3 item 1' },
+            {
+              label: 'Level 3 item 2',
+              link: '/',
+              onClick: (e, item) => console.log('Click and link', e, item),
+            },
+            {
+              label: 'Level 3 item 3',
+              tree: [
+                {
+                  label: 'Level 4 item 1 (outside link)',
+                  link: 'https://www.google.com',
+                  target: '_blank',
+                },
+                { label: 'Level 4 item 2 (routeLink)', routeLink: '/basic-components' },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ];
 
   parent.addDraw(new MenuTree({ attachId, tree: menuTree1 }));
+
+  parent.addDraw(breakProps);
+  parent.addDraw(new MenuTree({ attachId, tree: menuTree2 }));
 };
 
 export default MenuTreeExample;
